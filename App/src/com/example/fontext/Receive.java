614b6ba359d6f5046f1e6e11276dc47e950ac770
Benjamin Kaiser <benjamin.h.kaiser@gmail.com>
2013-01-07 11:42:12 -0500

    Basic inbox view fleshed out
    Refresh button in inbox now updates listview with all recieved messages.
    Each item in the listview can be interacted with - a touch displays a
    toast containing the full message info.
    Messages are not sorted into threads yet.

--- App/src/com/example/fontext/Receive.java
+++ App/src/com/example/fontext/Receive.java
@@ -1,10 +1,19 @@
 package com.example.fontext;
 
+import java.util.ArrayList;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class Receive extends Activity {
 	
@@ -30,4 +39,56 @@ public class Receive extends Activity {
 		startActivity(intent);
 	}
 
+	//Global arraylist containing all SMSs.
+    ArrayList<String> smsList = new ArrayList<String>();
+    
+    
+    /**
+     * Refreshes listview with all SMS's in database. Called upon refresh btn press.
+     * @param view view of the Receive activity
+     */
+	public void refreshMsgs(View view) {
+		//Instantiate a contentResolver to access SMS's stored in sms database
+		ContentResolver contentResolver = getContentResolver();
+		Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
+
+		//Get indexes of message body and sender address
+		int indexBody = cursor.getColumnIndex(SmsReceiver.BODY);
+		int indexAddr = cursor.getColumnIndex(SmsReceiver.ADDRESS);
+		
+		//If there are no messages, return
+		if (indexBody<0 || !cursor.moveToFirst()) return;
+		
+		//Clear list of existing messages
+		smsList.clear();
+		
+		//For every message in the database, create a string and add it to the smsList
+		do{
+			String message = "Sender: " + cursor.getString( indexAddr ) + "\n" + cursor.getString( indexBody );
+			smsList.add(message);
+		}
+		while(cursor.moveToNext());
+
+		//Get the listView and create an Adapter to convert each string into a list item
+		ListView smsListView = (ListView) findViewById(R.id.lstInbox);
+		smsListView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, smsList));
+		
+		//Create the listener for list item clicks
+		smsListView.setOnItemClickListener(new OnItemClickListener(){
+			//On clicks, separate string into body and sender and print toast
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
+        		try{
+        		    String[] splitted = smsList.get(pos).split("\n"); 
+        			String data = splitted[0] + "\n";
+        			for (int i=1; i<splitted.length; ++i)
+        			    data += splitted[i];
+        			
+        			Toast.makeText(getBaseContext(), data, Toast.LENGTH_SHORT ).show();
+        		}	//close try
+        		catch (Exception e) { e.printStackTrace(); }
+        	}	//close onItemClick()
+        });	//close OnItemClickListener, then close setOnItemClickListener() and end line
+	}
+	
 }
--- App/src/com/example/fontext/SmsReceiver.java
+++ App/src/com/example/fontext/SmsReceiver.java
@@ -1,15 +1,39 @@
 package com.example.fontext;
 
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
 import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver{
 	public static final String SMS_EXTRA_NAME = "pdus";
+	public static final String SMS_URI = "content://sms";
 	
+	public static final String ADDRESS = "address";
+    public static final String PERSON = "person";
+    public static final String DATE = "date";
+    public static final String READ = "read";
+    public static final String STATUS = "status";
+    public static final String TYPE = "type";
+    public static final String BODY = "body";
+    public static final String SEEN = "seen";
+    
+    public static final int MESSAGE_TYPE_INBOX = 1;
+    public static final int MESSAGE_TYPE_SENT = 2;
+    
+    public static final int MESSAGE_IS_NOT_READ = 0;
+    public static final int MESSAGE_IS_READ = 1;
+    
+    public static final int MESSAGE_IS_NOT_SEEN = 0;
+    public static final int MESSAGE_IS_SEEN = 1;
+		
+	//TODO: After figuring out how to disable stock SMS app, remove warning supression    
+	@SuppressWarnings("unused")
 	@Override
 	public void onReceive(Context context, Intent intent) {
 		// Get the SMS map from Intent
@@ -17,10 +41,13 @@ public class SmsReceiver extends BroadcastReceiver{
         String messages = "";
         
         if (extras != null){
-            // Get received SMS array
+            //Get received SMS array
             Object[] smsExtra = (Object[]) extras.get( SMS_EXTRA_NAME );
             
-            //Concatentate
+            //Get ContentResolver object for pushing encrypted SMS to incoming folder
+            ContentResolver contentResolver = context.getContentResolver();
+            
+            //Get messages from bundle
             for (int i=0; i<smsExtra.length; ++i){
                 SmsMessage sms = SmsMessage.createFromPdu((byte[])smsExtra[i]);
                  
@@ -29,10 +56,36 @@ public class SmsReceiver extends BroadcastReceiver{
                  
                 messages += "SMS from " + address + " :\n";                    
                 messages += body + "\n";
+                
+                /*TODO: Stop stock SMS app from receiving SMS's and storing to database.
+                 * For the meantime, or maybe permanently if that's a better solution, just
+                 * don't do it here.        
+                 */
+                //putSmsToDatabase(contentResolver, sms);
             }
              
             // Display toast containing message
             Toast.makeText(context, messages, Toast.LENGTH_SHORT ).show();
         }
     }
+	
+	//TODO: After figuring out how to disable stock SMS app, remove warning supression
+	@SuppressWarnings("unused")
+	private void putSmsToDatabase(ContentResolver contentResolver, SmsMessage sms) {
+		// Create SMS row
+        ContentValues values = new ContentValues();
+        values.put(ADDRESS, sms.getOriginatingAddress());
+        values.put(DATE, sms.getTimestampMillis());
+        values.put(READ, MESSAGE_IS_NOT_READ);
+        values.put(STATUS, sms.getStatus());
+        values.put(TYPE, MESSAGE_TYPE_INBOX);
+        values.put(SEEN, MESSAGE_IS_NOT_SEEN);
+        
+        try { values.put(BODY, sms.getMessageBody()); } 
+        catch (Exception e) { e.printStackTrace(); }
+        
+        // Push row into the SMS table
+        contentResolver.insert(Uri.parse(SMS_URI), values);
+	}
 }
+
