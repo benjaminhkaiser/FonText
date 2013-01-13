b75a0e4cbfc1e42c92fcfc3ad530c35a684abcaf
Benjamin Kaiser <benjamin.h.kaiser@gmail.com>
2013-01-13 13:41:03 -0500

    Beginning to implemented threaded messaging
    Inbox now shows only most recent message from each contact instead of
    all messages. This isn't very exciting, but it's a start towards
    threaded messaging.

--- App/src/com/example/fontext/Inbox.java
+++ App/src/com/example/fontext/Inbox.java
@@ -43,41 +43,50 @@ public class Inbox extends Activity {
 	}
 
 	//Global arraylist containing all SMSs.
-    ArrayList<Spanned> smsList = new ArrayList<Spanned>();
+    ArrayList<Spanned> conversationList = new ArrayList<Spanned>();
     
     /**
      * Refreshes listview with all SMS's in database. Called upon refresh btn press.
      * @param view view of the Inbox activity
      */
 	public void refreshMsgs(View view) {
-		//Instantiate a contentResolver to access SMS's stored in sms database
-		ContentResolver contentResolver = getContentResolver();
-		Cursor cursor = contentResolver.query(Uri.parse("content://sms/inbox"), null, null, null, null);
-
-		//Get indexes of message body and sender address
-		int indexBody = cursor.getColumnIndex(SmsReceiver.BODY);
-		int indexAddr = cursor.getColumnIndex(SmsReceiver.ADDRESS);
 		
-		//If there are no messages, return
-		if (indexBody<0 || !cursor.moveToFirst()) return;
+		//Instantiate a contentResolver to access SMS conversations
+		ContentResolver contentResolver = getContentResolver();
+		Cursor conversationCursor = contentResolver.query(Uri.parse("content://sms/conversations"), null, null, null, "date desc");
 		
 		//Clear list of existing messages
-		smsList.clear();
-			
-		//For every message in the database, create a string and add it to the smsList
-		do{
-			//get message and sender and convert shortcode in body to HTML tags
-			String body = Compose.decodeMessage(cursor.getString(indexBody));
-			String sender = SmsReceiver.getContactbyNumber(cursor.getString(indexAddr), this);
-			String message = sender + ": " + body;
+		conversationList.clear();
 		
-			smsList.add(Html.fromHtml(message));
+		//If there are no conversations, return.
+		if (!conversationCursor.moveToFirst()) return;
+			
+		//For every converation in the database, ...
+		do{
+			//get thread id, then get inbox messages related to that id 
+			String where = "thread_id=" + conversationCursor.getString(conversationCursor.getColumnIndex("thread_id"));
+		    Cursor inboxCursor= getContentResolver().query(Uri.parse("content://sms/inbox"), null, where, null, "date desc"); 
+		    String address = "";
+		    
+		    //get address of conversation partner
+		    if (inboxCursor.moveToFirst()){
+		    	address = inboxCursor.getString(inboxCursor.getColumnIndexOrThrow("address")).toString();
+
+		    	//get message and sender and convert shortcode in body to HTML tags
+		    	String body = Compose.decodeMessage(inboxCursor.getString(inboxCursor.getColumnIndex("body")));
+				String sender = SmsReceiver.getContactbyNumber(address, this);
+				String message = sender + ": " + body;
+				
+				conversationList.add(Html.fromHtml(message));
+		    }
+		
+			
 		}
-		while(cursor.moveToNext());
+		while(conversationCursor.moveToNext());
 
 		//Get the listView and create an Adapter to convert each string into a list item
 		ListView smsListView = (ListView) findViewById(R.id.lstInbox);
-		smsListView.setAdapter(new ArrayAdapter<Spanned>(this, android.R.layout.simple_list_item_1, smsList));
+		smsListView.setAdapter(new ArrayAdapter<Spanned>(this, android.R.layout.simple_list_item_1, conversationList));
 		
 		//Create the listener for list item clicks
 		smsListView.setOnItemClickListener(new OnItemClickListener(){
@@ -85,7 +94,7 @@ public class Inbox extends Activity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int pos, long id){
         		try{
-        			String message = Html.toHtml(smsList.get(pos)).replace("<p dir=ltr>", "").replace("</p>", "");
+        			String message = Html.toHtml(conversationList.get(pos)).replace("<p dir=ltr>", "").replace("</p>", "");
         		    String[] splitted = message.split("\n"); 
         			String data = splitted[0] + "\n";
         			for (int i=1; i<splitted.length; ++i)
